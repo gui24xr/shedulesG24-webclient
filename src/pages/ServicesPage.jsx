65d7a211c268a08a168cc5549bc8c10886daa81f
gui24xr <guillermoxr24@gmail.com>
2025-02-14 17:44:35 -0300

    2

---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const ServicesPage = () => {
+    const {companyId} = useParams()
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default ServicesPage;
